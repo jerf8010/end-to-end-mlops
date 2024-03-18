3190fafd06b624e3ae2dc6866654b9a05dca7ee8
Emanuel Fitta <jerf8010@gmail.com>
2024-03-18 12:09:59 -0600

    05_model_evaluation stage created

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline  # noqa: E501
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline  # noqa: E501
 from mlProject.pipeline.stage_04_data_trainer import DataTrainerTrainingPipeline  # noqa: E501
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline  # noqa: E501
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -43,6 +44,17 @@ STAGE_NAME = "Data Trainer Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
