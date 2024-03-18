6a932383389de43551ca26f689aaecd22f1a39d1
Emanuel Fitta <jerf8010@gmail.com>
2024-03-18 03:15:49 -0600

    03_data_transformation stage created

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline  # noqa: E501
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline  # noqa: E501
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline  # noqa: E501
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -13,12 +15,22 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-
 STAGE_NAME = "Data Validation Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
---
+++ src/mlProject/component/data_transformation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from mlProject import logger
+from mlProject.entity.config_entity import DataTransformationConfig
+
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+
+    def train_test_spliting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        train, test = train_test_split(data)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)  # noqa: E501
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)  # noqa: E501
+
+        logger.info("Splited data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
